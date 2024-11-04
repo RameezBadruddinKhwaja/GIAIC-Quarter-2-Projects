7b979e2458cc485466064370bac7d0ff778effbf
RameezBadruddinKhwaja <161029832+RameezBadruddinKhwaja@users.noreply.github.com>
2024-11-04 12:34:57 +0500

    Delete Hackathon/Milestione 2 directory

--- Hackathon/Milestione 2/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function toggleSection(contentId) {
-    var content = document.getElementById(contentId);
-    if (content) {
-        content.classList.toggle('hidden');
-    }
-}
