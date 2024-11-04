db35545215f435be3f1e646375a07e410705c1c2
RameezBadruddinKhwaja <rameezbader@gmail.com>
2024-11-04 13:03:45 +0500

    Updated file names and made minor changes to content

--- Hackathon/Milestione 2/script.ts
+++
@@ -1,8 +0,0 @@
-function toggleSection(contentId: string): void {
-    const content = document.getElementById(contentId);
-    if (content) {
-        content.classList.toggle('hidden');
-    }
-}
-
-export {}; // This ensures the file is treated as a module
