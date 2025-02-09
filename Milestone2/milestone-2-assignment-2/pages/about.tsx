64aa7e662665cf43b6f765ab70c0531db08df4b1
RameezBadruddinKhwaja <161029832+RameezBadruddinKhwaja@users.noreply.github.com>
2025-02-09 18:22:01 +0500

    Delete Milestone2/milestone-2-assignment-2 directory

--- Milestone2/milestone-2-assignment-2/pages/about.tsx
+++
@@ -1,3 +0,0 @@
-export default function About() {
-    return <h1 className="text-3xl font-bold text-green-700">About Us</h1>;
-  }
--- Milestone2/milestone-2-assignment-2/pages/contact.tsx
+++
@@ -1,4 +0,0 @@
-export default function Contact() {
-    return <h1 className="text-3xl font-bold text-red-700">Contact Us</h1>;
-  }
-  
